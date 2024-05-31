700e01a2943f48b2b2182330282d3bb199899d0a
iankuria668 <iankuria668@gmail.com>
2024-05-31 14:50:23 +0300

    Solved

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,5 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+    def match(self, candidates):
+        return [w for w in candidates if self.word != w and sorted(w) == sorted(self.word)]
